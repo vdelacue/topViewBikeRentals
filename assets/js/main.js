11d96eb286d2e22628dc4873608ed5f9f926ab24
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-09-23 21:11:35 -0400

    styling landing and creating responsiveness

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+// data provided
+
+const products =
+{
+    "products": [
+      {
+        "id": 1,
+        "name": "Adult Male Bike",
+        "price": 20.50,
+        "image": "http://via.placeholder.com/250x250?text=Adult%20Male%20Bike",
+        "product_type": "bike"
+      },
+      {
+        "id": 2,
+        "name": "Adult Female Bike",
+        "price": 20.50,
+        "image": "http://via.placeholder.com/250x250?text=Adult%20Female%20Bike",
+        "product_type": "bike"
+      },
+      {
+        "id": 3,
+        "name": "Kids Unisex Bike",
+        "price": 12.75,
+        "image": "http://via.placeholder.com/250x250?text=Kids%20Unisex%20Bike",
+        "product_type": "bike"
+      },
+      {
+        "id": 4,
+        "name": "Adult Unisex Helmet",
+        "price": 4.00,
+        "image": "http://via.placeholder.com/250x250?text=Adult%20Unisex%20Helmet",
+        "product_type": "accessory"
+      },
+      {
+        "id": 5,
+        "name": "Kids Unisex Helmet",
+        "price": 3.50,
+        "image": "http://via.placeholder.com/250x250?text=Kids%20Unisex%20Helmet",
+        "product_type": "accessory"
+      },
+      {
+        "id": 6,
+        "name": "Insurance",
+        "price": 9.99,
+        "image": "http://via.placeholder.com/250x250?text=Insurance",
+        "product_type": "addon"
+      }
+    ]
+  };
+
